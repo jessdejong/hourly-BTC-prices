07361901f0e154c7ec7d804470a87aab8c696953
Jess <jessdejong843@gmail.com>
2018-01-03 05:25:19 +0000

    new file:   BitcoinPriceTwitterBot.py 	new file:   LICENSE.txt 	new file:   MANIFEST.in 	modified:   README.md 	new file:   auth.py 	new file:   exchanges/__init__.py 	new file:   exchanges/__pycache__/__init__.cpython-35.pyc 	new file:   exchanges/__pycache__/base.cpython-35.pyc 	new file:   exchanges/__pycache__/bitfinex.cpython-35.pyc 	new file:   exchanges/__pycache__/bitstamp.cpython-35.pyc 	new file:   exchanges/__pycache__/bitvc.cpython-35.pyc 	new file:   exchanges/__pycache__/bravenewcoin.cpython-35.pyc 	new file:   exchanges/__pycache__/coinapult.cpython-35.pyc 	new file:   exchanges/__pycache__/coindesk.cpython-35.pyc 	new file:   exchanges/__pycache__/futures796.cpython-35.pyc 	new file:   exchanges/__pycache__/helpers.cpython-35.pyc 	new file:   exchanges/__pycache__/huobi.cpython-35.pyc 	new file:   exchanges/__pycache__/kraken.cpython-35.pyc 	new file:   exchanges/__pycache__/okcoin.cpython-35.pyc 	new file:   exchanges/__pycache__/poloniex.cpython-35.pyc 	new file:   exchanges/base.py 	new file:   exchanges/bitfinex.py 	new file:   exchanges/bitmex.py 	new file:   exchanges/bitstamp.py 	new file:   exchanges/bitvc.py 	new file:   exchanges/bravenewcoin.py 	new file:   exchanges/coinapult.py 	new file:   exchanges/coindesk.py 	new file:   exchanges/futures796.py 	new file:   exchanges/helpers.py 	new file:   exchanges/huobi.py 	new file:   exchanges/kraken.py 	new file:   exchanges/okcoin.py 	new file:   exchanges/poloniex.py 	new file:   requirements.txt 	new file:   setup.cfg 	new file:   setup.py

---
+++ BitcoinPriceTwitterBot.py
@@ -0,0 +1,17 @@
+from twython import Twython
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+message = "Hi"
+twitter.update_status(status=message)
+print("Tweeted: {}".format(message))
